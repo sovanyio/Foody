f11f9d70f4871acc269df822ccb029fc3b1a689a
Brian Bolton <sovanyio@gmail.com>
2015-03-10 21:19:51 -0500

    Add production db info

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => 'mysql.brianwb.com',
+			'database'  => 'foody_sr27',
+			'username'  => 'foody_sr27',
+			'password'  => 'Laser@Mail!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
